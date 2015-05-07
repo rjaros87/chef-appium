b4b2ce9a577d1c7f52e640317e0aa0e1268eea06
Dennis Hoer <dennis.hoer@pearson.com>
2015-05-06 17:01:26 -0700

    Tweak readme

--- metadata.rb
+++ metadata.rb
@@ -2,14 +2,10 @@ name 'appium'
 maintainer 'Dennis Hoer'
 maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
-description 'Installs/Configures appium'
+description 'Installs/Configures Appium'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.2.5'
+version '0.1.0'
 
-supports 'centos'
-supports 'redhat'
 supports 'mac_os_x'
-supports 'ubuntu'
-supports 'windows'
 
 depends 'nodejs'
